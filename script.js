d479cda99c0d266c74f94c0be98933e8374fec38
Sandeep Subedi <sandeep1111able@gmail.com>
2023-01-03 17:27:55 +0545

    Refactor playRound function and add game function for 5 rounds play

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-const getComputerChoice = function(){
-    computerOptions =['rock','paper','scissors'];
-    return computerOptions[Math.floor(Math.random()*3)];
-}
+const getComputerChoice = function () {
+  computerOptions = ["rock", "paper", "scissors"];
+  return computerOptions[Math.floor(Math.random() * 3)];
+};
 
-const playRound = function(playerSelection,computerSelection){
-playerSelection = playerSelection.toLowerCase();
-if(playerSelection =='paper' && computerSelection == 'rock' || playerSelection =='scissors' && computerSelection == 'paper'|| playerSelection =='rock' && computerSelection == 'scissors'){
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
-}
+let playerCount = 0;
+let computerCount = 0;
 
-else if(computerSelection =='paper' && playerSelection == 'rock' || computerSelection =='scissors' && playerSelection == 'paper'|| computerSelection =='rock' && playerSelection == 'scissors'){
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
-}
-else{
-    return `It was a Draw! ${computerSelection} draws ${playerSelection}`
-}
+const playRound = function (playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  if (
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper") ||
+    (playerSelection == "rock" && computerSelection == "scissors")
+  ) {
+    console.log(
+      `You Won this round! ${playerSelection} beats ${computerSelection}`
+    );
+    return playerCount++;
+  } else if (
+    (computerSelection == "paper" && playerSelection == "rock") ||
+    (computerSelection == "scissors" && playerSelection == "paper") ||
+    (computerSelection == "rock" && playerSelection == "scissors")
+  ) {
+    console.log(
+      `You Lose this round! ${computerSelection} beats ${playerSelection}`
+    );
+    return computerCount++;
+  } else {
+    console.log(
+      `You drew this round! ${computerSelection} draws ${playerSelection}`
+    );
+  }
+};
 
+const game = function () {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Please choose your weapon");
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+  }
+  if (playerCount > computerCount) {
+    console.log(
+      `You are the winner, you won ${playerCount} rounds and computer won ${computerCount} rounds out of 5 rounds`
+    );
+  } else {
+    console.log(
+      `You are the loser, you won ${playerCount} rounds and computer won ${computerCount} rounds out of 5 rounds`
+    );
+  }
+};
 
-
-
-}
-
-const playerSelection = "scissors";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+game();
